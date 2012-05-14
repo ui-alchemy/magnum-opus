7875f2602a937252affab61a21d6f42a3aaf7d91
Eric D Helms <ehelms@redhat.com>
2012-05-14 16:55:17 -0400

    Updates to latest converge-ui and adds support for displaying the login page layout, and password reset page layouts.

--- app/controllers/login_controller.rb
+++ app/controllers/login_controller.rb
@@ -8,4 +8,8 @@ class LoginController < ApplicationController
     render "index", :layout => "converge-ui/login_layout"
   end
 
+  def password_reset
+    render "password_reset", :layout => "converge-ui/password_reset_layout"
+  end
+
 end
