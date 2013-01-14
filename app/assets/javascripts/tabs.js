00fc6f187c046cc0d4ed3eb2b8a0af9d6851ab69
Imre Farkas <ifarkas@redhat.com>
2013-01-14 16:41:28 +0100

    DevGuide - Fix tabs for forms layout

--- app/assets/javascripts/tabs.js
+++ app/assets/javascripts/tabs.js
@@ -1,12 +1,34 @@
 $(function(){
-    $('a[data-component="tab"]').live('click', function(){
-        var href = $(this).attr('href');
 
-        $('div[data-component="tab-container"]').hide();
-        $('a[data-component="tab"]').parent().removeClass('selected');
+    $('a[data-component="code-tab"]').live('click', function(e){
+        e.preventDefault();
 
+        var $section = $(this).closest('section');
+        $section.find('a[data-component="code-tab"]').parent().removeClass('selected');
         $(this).parent().addClass('selected');
-        $(href).show();
+
+        var type = $section.find('li.selected > a[data-component="example-tab"]').data('type');
+        var format = $(this).data('format');
+        showCodeExample($section, type, format);
     });
+
+    $('a[data-component="example-tab"]').live('click', function(e){
+        e.preventDefault();
+
+        $('a[data-component="example-tab"]').parent().removeClass('selected');
+        $(this).parent().addClass('selected');
+
+        var $section = $(this).closest('section');
+        $section.find($(this).data("target")).removeClass().addClass($(this).data("class"));
+
+        var type = $(this).data('type');
+        var format = $section.find('li.selected > a[data-component="code-tab"]').data('format');
+        showCodeExample($section, type, format);
+    });
+
+    function showCodeExample($section, type, format) {
+        $section.find('div[data-component="tab-container"]').hide();
+        $section.find('div[data-component="tab-container"][data-type=' + type + '][data-format=' + format + ']').show();
+    }
 
 });
